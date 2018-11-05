f690109472f917e6d5c2d8a841c071e20f18e2ab
olekturbo <aleks.szewczak@gmail.com>
2018-11-05 23:20:43 +0100

    Uzycie webpacka. Instalacja npm. Zmiana styli voyagera.

--- config/voyager.php
+++ config/voyager.php
@@ -205,7 +205,7 @@ return [
 
     // Here you can specify additional assets you would like to be included in the master.blade
     'additional_css' => [
-        //'css/custom.css',
+        'css/style.css',
     ],
 
     'additional_js' => [
