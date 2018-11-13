b4bb410d1aba699eb65444dd17ff84d35662bcb1
olekturbo <aleks.szewczak@gmail.com>
2018-11-13 01:16:55 +0100

    Add exception for authorization

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -52,10 +52,6 @@ class Handler extends ExceptionHandler
 
     protected function unauthenticated($request, AuthenticationException $exception)
     {
-        if ($request->expectsJson()) {
             return response()->json(['error' => 'Unauthenticated.'], 401);
-        }
-
-        return redirect()->guest(route('login'));
     }
 }
